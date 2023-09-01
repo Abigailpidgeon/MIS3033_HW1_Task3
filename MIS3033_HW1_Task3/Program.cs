320e42298e05762b579dfc1682e5fd3c95314b68
alp08 <alp08@BOOK-QJR6576NGV>
2023-09-01 11:36:59 -0500

    Add project files.

---
+++ MIS3033_HW1_Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// MIS3033
+// Task 3
+
+double totalscore = 0.0;
+int counter = 0;
+string answer;
+string mesg;
+
+do
+{
+    mesg = "Please enter your score: ";
+    Console.Write(mesg);
+    string scorestr = Console.ReadLine();
+    double score = Convert.ToDouble(scorestr);
+    counter++;
+    totalscore += score;
+
+    mesg = "Do you have another score to input? ";
+    Console.Write(mesg);
+    answer = Console.ReadLine();96.
+
+} while (answer.ToLower() == "yes");
+
+double avrgscore = totalscore / counter;
+mesg = String.Format("The number of scores enterd is: {0}\n",counter);
+Console.Write(mesg);
+mesg = String.Format("The average score is {0:F2}\n", avrgscore);
+Console.Write(mesg);
